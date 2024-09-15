0cba9d9a5bc93c902d703deb5ff4f8b0b0e2fc85
HGX Server <21020205@vnu.edu.vn>
2024-09-15 12:03:50 +0900

    update

--- eval.py
+++ eval.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 
-def get_model_list(path: str = "model/hf_model2.csv", search_key_word: list[str]= None):
+def get_model_list(path: str = "model/hf_model.csv", search_key_word: list[str]= None):
     """ 
     Inputs:
         path: csv_file contain config 
@@ -99,11 +99,10 @@ def main():
     # prompt_prefix = """Dựa vào trí nhớ của bạn về các bộ dữ liệu, hãy điền vào đoạn <MASKED> trong câu sau để hoàn thành 1 câu hỏi trắc nghiệm. """
     # prompt_suffix = ""
     os.environ["CUDA_VISIBLE_DEVICES"]="4,5"
-    
     DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(DEVICE)
-    BATCH_SIZE = 32
-    evaluate(model_names= None, data_names = "all_filter_data",  process_types= ["mask_wrong_answer", "mask_half_question"], 
+    BATCH_SIZE = 10
+    evaluate(model_names= None, data_names = ["final_domain_ver1", "eng_final_domain_ver1"],  process_types= ["mask_wrong_answer", "mask_half_question"], 
              prompt_prefix= None, prompt_suffix= None,size = None,
              intermediate_data_save_path= None, DEVICE= DEVICE, BATCH_SIZE= BATCH_SIZE)
             
